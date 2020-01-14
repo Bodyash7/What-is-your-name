cb1cbf013a2c08f975bcb9a4c851d3fa8d3c5b4a
Bogdan <49554577+Bodyash7@users.noreply.github.com>
2020-01-14 12:59:13 +0200

    Create What is your name.py

---
+++ What is your name.py
@@ -0,0 +1,9 @@
+a = (input ("What is your name?: "))
+b = (input ("How old are you?: "))
+c = (input ("Where do you live?: "))
+
+
+print (" ")
+print ("Hello,", a)
+print ("Your age is", b)
+print ("You live in", c)
